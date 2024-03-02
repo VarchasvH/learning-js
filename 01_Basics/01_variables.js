e227ddae7e54bcc047fdb7917bb8fce4de90b7ec
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-02 16:09:15 +0000

    First File

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+// Constants
+const accountID = 414141;
+let accountEmail = "varchasv.code@gmail.com";
+var accountPassword = "Pass@123";
+accountCity = "Noida";
+
+// Cannot be changed once defined
+accountID = 2;
+accountEmail = "varchasv@gmail.com";
+
+
+console.log(accountID);
