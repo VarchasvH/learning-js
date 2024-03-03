1de62a5ac1d516065d30b2ac7fd68f7ae3e0742b
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-03 06:16:55 +0000

    operations

--- 01_Basics/03_dataType_conversion.js
+++ 01_Basics/03_dataType_conversion.js
@@ -1,4 +1,4 @@
-// Number Conversion
+// // Number Conversion
 let score = "20";
 console.log(typeof score);
 console.log(typeof (score));
@@ -7,30 +7,52 @@ let value_in_Number = Number(score);
 console.log(typeof value_in_Number);
 console.log(value_in_Number);
 
-/* Output
-    Don't rely on Number to change the data type.
-    "33" => 33
-    "33abc" => NaN
-    true => 1, false => 0
-    null => 0
-    undefined => NaN
-*/
+// /* Output
+//     Don't rely on Number to change the data type.
+//     "33" => 33
+//     "33abc" => NaN
+//     true => 1, false => 0
+//     null => 0
+//     undefined => NaN
+// */
 
-// Boolean conversion
+// // Boolean conversion
 let isloggedIn = 1;
 
 let booleanIsloggedIn = Boolean(isloggedIn);
 console.log(booleanIsloggedIn);
 
-/* Output
-    1 => true; 0 => false;
-    "" => false;
-    "varchasv" => true;
-*/
+// /* Output
+//     1 => true; 0 => false;
+//     "" => false;
+//     "varchasv" => true;
+// */
 
-// String Conversion
+// // String Conversion
 let someNumber = 30;
 let StringNumber = String(someNumber);
 
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+
+/*------------------------------------------------OPERATIONS--------------------------------------------------*/ 
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+let str1 = "hello";
+let str2 = "world";
+let str3 = str1 + " " +str2;
+// console.log(str3);
+
+console.log("2" + 1);
+console.log(2 + "1");
+console.log("2" + "1");
+console.log("2" + 1 + 3); // String first, then we treat all as strings
+console.log(2 + 3 + "1"); // String last, we treat all others as numbers before the string starts
+
+
+
+
