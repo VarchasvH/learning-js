68c4b10790d805f2d2d3c1d4ad079bdaabbe0a7c
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-03 04:57:21 +0000

    first file

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,12 +1,14 @@
-// Constants
 const accountID = 414141;
 let accountEmail = "varchasv.code@gmail.com";
+// Do no use Var, It does not understand the scope
 var accountPassword = "Pass@123";
 accountCity = "Noida";
 
-// Cannot be changed once defined
-accountID = 2;
+// accountID = 2; Constants cannot be changed.
 accountEmail = "varchasv@gmail.com";
-
+accountPassword = "212121";
+accountCity = "Delhi";
 
 console.log(accountID);
+console.table([accountEmail, accountPassword, accountCity]);
+
