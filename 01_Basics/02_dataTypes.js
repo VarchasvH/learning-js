4c27967b2ace5adb71a72d4dd69379ddfe8c7937
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-03 05:16:42 +0000

    second file

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// Make sure that the code is readable
+
+"use strict"; // Treat all code as newer JavaScript
+// alert("Hello!"); // Cannot be used, we are not inside a browser
+
+// Primitive Data types
+let age = 21; // Number
+let name = "Varchasv"; // String
+let isMale = true; // Boolean
+let temprature = null; // standalone object
+let income; // undefined
+
+// Object Data Types
+
+
+
+console.log(typeof temprature);
