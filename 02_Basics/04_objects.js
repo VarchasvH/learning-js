3df598795323594298bd1a9eca39c7b7b771c659
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-04 09:49:28 +0000

    Video 18 done

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,52 +1,68 @@
-// const tinderUser = {}; // Non-Singleton object
+// Video - 17
+// // const tinderUser = {}; // Non-Singleton object
 
-const tinderUser = new Object(); // Singleton object
+// const tinderUser = new Object(); // Singleton object
 
-tinderUser.id = "test";
-tinderUser.name = "Varchasv";
-tinderUser.isLoggedIn = false;
+// tinderUser.id = "test";
+// tinderUser.name = "Varchasv";
+// tinderUser.isLoggedIn = false;
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-const regularUser = {
-    email: "varchasv@india.com",
-    fullName: {
-        firstName: "Varchasv",
-        lastName: "Hoon"
-    },
+// const regularUser = {
+//     email: "varchasv@india.com",
+//     fullName: {
+//         firstName: "Varchasv",
+//         lastName: "Hoon"
+//     },
+// };
+
+// // console.log(regularUser.fullName.lastName);
+
+// const Obj1 = {1: 'a', 2: 'b'};
+// const Obj2 = {3: 'a', 4: 'b'};
+
+// // +++++++++++++ Combine Objects +++++++++++++ // 
+// const Obj3 = {...Obj1,...Obj2}; // Using Spread
+// // console.log(Obj3);
+
+// const Obj4 = Object.assign({}, Obj1, Obj2); // Using Assign, the first one is the target
+// // console.log(Obj4);
+
+// // You can also clone an object like this
+// const cloneObj = Object.assign({}, Obj1); // Cloned Obj1 into cloneObj
+
+
+// // +++++++++ Multiple Objects inside an Array ++++++++ \\
+// const MultipleObjects = [
+//     {
+//         id: 1,
+//         email: "foo@example.com"
+//     },
+//     {
+
+//     },
+// ];
+
+// console.log(MultipleObjects[0].email);
+
+// // +++++++ GET KEYS and Values +++++++++\\
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // Convert each key/value pair into an array
+// console.log(tinderUser.hasOwnProperty("Varchasv"));
+
+// +++++++++++++++++++++++++ Destructuring Objects +++++++++++++++++++++ \\
+
+const course = {
+    name : "JavaScript Course in Hindi",
+    price : "999 INR",
+    instructor : "Hitesh Chaudhary",
+    companyName : "ChaiAurCode"
 };
 
-// console.log(regularUser.fullName.lastName);
+// We can deconstruct the elements into a variable or constant so that we can use them easily
+const {instructor: teacher} = course; // instructor was changed to teacher and can be used as such
+const {price :discountedPrice} = course; // price was changed to discounted price
 
-const Obj1 = {1: 'a', 2: 'b'};
-const Obj2 = {3: 'a', 4: 'b'};
-
-// +++++++++++++ Combine Objects +++++++++++++ // 
-const Obj3 = {...Obj1,...Obj2}; // Using Spread
-// console.log(Obj3);
-
-const Obj4 = Object.assign({}, Obj1, Obj2); // Using Assign, the first one is the target
-// console.log(Obj4);
-
-// You can also clone an object like this
-const cloneObj = Object.assign({}, Obj1); // Cloned Obj1 into cloneObj
-
-
-// +++++++++ Multiple Objects inside an Array ++++++++ \\
-const MultipleObjects = [
-    {
-        id: 1,
-        email: "foo@example.com"
-    },
-    {
-
-    },
-];
-
-console.log(MultipleObjects[0].email);
-
-// +++++++ GET KEYS and Values +++++++++\\
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser)); // Convert each key/value pair into an array
-console.log(tinderUser.hasOwnProperty("Varchasv"));
+console.log(`${teacher} is offering the course at ${discountedPrice}.`);
